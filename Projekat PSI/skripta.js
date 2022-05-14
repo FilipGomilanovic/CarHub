d205b8f89251b279069c55d6ab71f203460e1cbf
milos01599 <75314651+milos01599@users.noreply.github.com>
2022-05-15 01:39:01 +0200

    Pocetak implementacije

---
+++ Projekat PSI/skripta.js
@@ -0,0 +1,17 @@
+
+$(document).ready( function()
+{
+    $("#logo").hide();
+    $(".btn-danger").hide();
+    
+
+   
+    $("#logo").show(3000);
+    $(".btn-danger").show(3000);
+   
+
+
+
+
+
+})
