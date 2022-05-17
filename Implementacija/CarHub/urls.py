a94e5227eb0850d87c62fc653ba45de6721296c2
Milos Mijailovic <milosmijailovic99@gmail.com>
2022-05-17 17:15:13 +0200

    Pocetna strana, prijava, registracija, Pocetna strana ulogovanog korisnika, odjavljivanje

--- Implementacija/CarHub/urls.py
+++ Implementacija/CarHub/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name="CarHub"
+
 urlpatterns = [
-    path('pathTest', views.Test),
-    path('postavljanjeOglasa', views.postavljanjeOglasa)
+    path('pathTest', views.Test, name="pocetnaStrana"),
+    path('pathTestUlogovan', views.Ulogovan, name="pocetnaStranaUlogovan"),
+
+    path('postavljanjeOglasa', views.postavljanjeOglasa),
+    path('prijava',views.prijava),
+    path('registracija',views.registracija),
+    path('logout',views.logout,name="logout")
+
 ]
