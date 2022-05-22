94edb5013495f6ae4744d2922ad67b8a794fe2d1
Ognjen <ognjen.stanojevic321@gmail.com>
2022-05-22 04:58:24 +0200

    Dodata tabela Slike, koja omogucava dodavanje vise slika za jedan oglas. Dodato dohvatanje podataka za konkretan oglas po id-ju. Namester carousel da radi sa djangom.

--- Implementacija/CarHub/migrations/0006_remove_model_karoserija_remove_model_kilometraza_and_more.py
+++ Implementacija/CarHub/migrations/0006_remove_model_karoserija_remove_model_kilometraza_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('CarHub', '0004_alter_oglas_model_idmodel_alter_oglas_table'),
+        ('CarHub', '0005_alter_oglas_model_idmodel_alter_oglas_table'),
     ]
 
     operations = [
