341c2fd78d393d27188c09c047be9fe940534ea0
Milos Mijailovic <milosmijailovic99@gmail.com>
2022-05-26 01:45:15 +0200

    Sitne izmene na lokalnoj grani

--- Implementacija/static/skripta.js
+++ Implementacija/static/skripta.js
@@ -9,7 +9,7 @@ $(document).ready( function()
     $("#logo").show(3000);
     $(".btn-danger").show(3000);
 
-    $("#sell").show();
+    $("#sell").hide();
     $("#rent").hide();
     $("#radio1, #radio2").change(function () {
         if ($("#radio1").is(":checked")) {
